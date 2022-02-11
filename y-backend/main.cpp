d6c5cc4d9a1b256a914f22cdef9519885e02eecf
Max <m@ax.mk>
2022-02-12 01:39:00 +0200

    Added libpq

--- y-backend/main.cpp
+++ y-backend/main.cpp
@@ -1,6 +1,24 @@
 #include <drogon/drogon.h>
+#include "third_party/libpq/libpq-fe.h"
 
 int main() {
+    auto _conn = PQsetdbLogin("127.0.0.1",
+        "5432",
+        NULL,
+        NULL,
+        "db",
+        "login",
+        "password");
+
+    const auto connection_status = PQstatus(_conn);
+
+    if(connection_status != ConnStatusType::CONNECTION_OK) {
+        std::cout << "Could not connect to the database!\n" << std::endl;
+        return 1;
+    } else {
+        std::cout << "Connected succesfully!\n" << std::endl;
+    }
+
     drogon::app().registerHandler("/test",
         [](const drogon::HttpRequestPtr& req,
             std::function<void (const drogon::HttpResponsePtr &)> &&callback)
