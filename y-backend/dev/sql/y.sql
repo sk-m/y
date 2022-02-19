bbb4ebe11f7aa52491bbe44dab3410011c177f0a
Max <m@ax.mk>
2022-02-20 00:38:38 +0200

    DB: set user->user_password size to 512

--- y-backend/dev/sql/y.sql
+++ y-backend/dev/sql/y.sql
@@ -5,6 +5,8 @@
 -- Dumped from database version 13.4
 -- Dumped by pg_dump version 13.3
 
+-- Started on 2022-02-20 00:36:00
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -21,18 +23,20 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- TOC entry 201 (class 1259 OID 22603)
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
     user_id integer NOT NULL,
     user_username character varying(128) NOT NULL,
-    user_password character varying(1024) NOT NULL,
+    user_password character varying(512) NOT NULL,
     user_last_login time with time zone
 );
 
 
 --
+-- TOC entry 200 (class 1259 OID 22601)
 -- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -46,6 +50,8 @@ CREATE SEQUENCE public.users_user_id_seq
 
 
 --
+-- TOC entry 2993 (class 0 OID 0)
+-- Dependencies: 200
 -- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -53,6 +59,7 @@ ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
 
 
 --
+-- TOC entry 2855 (class 2604 OID 22606)
 -- Name: users user_id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -60,27 +67,15 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY public.users (user_id, user_username, user_password, user_last_login) FROM stdin;
-\.
-
-
---
--- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
-
-
---
+-- TOC entry 2857 (class 2606 OID 22611)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
 
+
+-- Completed on 2022-02-20 00:36:04
 
 --
 -- PostgreSQL database dump complete
