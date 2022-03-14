b0e111e6a68ee5bceee858951abe6ebd62d4123c
Max <m@ax.mk>
2022-03-15 00:41:51 +0200

    Moved the aside menu into separate components

--- y-frontend/src/MainRoute.tsx
+++ y-frontend/src/MainRoute.tsx
@@ -2,6 +2,7 @@ import type { Component } from "solid-js";
 import { Route, Routes } from "solid-app-router";
 
 import "./app.css";
+import MainAsideMenu from "./components_internal/MainAsideMenu";
 
 const TestComponent: Component = () => {
     return (
@@ -13,73 +14,7 @@ const MainRoute: Component = () => {
     return (
         <div id="mainroute-container">
             <div id="mainroute-horizontal-container">
-                <div id="mainroute-aside-container">
-                    <div className="aside">
-                        <div className="content">
-                            <div className="userspace">
-                                <div className="main">
-                                    <div className="left">
-                                        <div className="user">
-                                            <div
-                                                className="user-avatar"
-                                                style={{ "background-image": "url(https://images.unsplash.com/photo-1604076913837-52ab5629fba9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)" }}
-                                            ></div>
-                                            <div className="user-username">admin</div>
-                                        </div>
-                                    </div>
-                                    <div className="right">
-                                        <div className="notifications-container has-unread">
-                                            <div className="notifications-count-container">
-                                                <div className="count">43</div>
-                                                <div className="icons">
-                                                    <span class="material-icons">warning</span>
-                                                    <span class="material-icons">question_mark</span>
-                                                    <span class="material-icons">north_east</span>
-                                                </div>
-                                            </div>
-                                            <div className="notifications-icon">
-                                                <div className="bubble"></div>
-                                                <span class="material-icons-round">notifications</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="menu">
-                                <div className="links">
-                                    <div className="link is-selected needs-attention">
-                                        <div className="left">
-                                            <div className="selection-indicator"></div>
-                                            <div className="icon"><span class="material-icons-round">tune</span></div>
-                                            <div className="text-container">
-                                                <div className="name">Settings</div>
-                                                <div className="description">System settings</div>
-                                            </div>
-                                        </div>
-                                        <div className="right">
-                                            <div className="needs-attention-bubble"></div>
-                                            <span class="arrow-icon material-icons-round">navigate_next</span>
-                                        </div>
-                                    </div>
-                                    <div className="link">
-                                        <div className="left">
-                                            <div className="selection-indicator"></div>
-                                            <div className="icon"><span class="material-icons-round">tune</span></div>
-                                            <div className="text-container">
-                                                <div className="name">Settings</div>
-                                                <div className="description">System settings</div>
-                                            </div>
-                                        </div>
-                                        <div className="right">
-                                            <div className="needs-attention-bubble"></div>
-                                            <span class="arrow-icon material-icons-round">navigate_next</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <MainAsideMenu />
 
                 <div>
                     <Routes>
---
+++ y-frontend/src/components_internal/MainAsideMenu.tsx
@@ -0,0 +1,91 @@
+import type { Component, PropsWithChildren } from "solid-js";
+
+const MenuLink: Component<{
+    icon_name: string,
+    name: string,
+    description: string,
+
+    is_selected?: boolean,
+    needs_attention?: boolean
+}> = props => {
+    return (
+        <div 
+            classList={{ link: true, "is-selected": props.is_selected, "needs-attention": props.needs_attention }}
+        >
+            <div className="left">
+                <div className="selection-indicator"></div>
+                <div className="icon"><span class="material-icons-round">{ props.icon_name }</span></div>
+                <div className="text-container">
+                    <div className="name">{ props.name }</div>
+                    <div className="description">{ props.description }</div>
+                </div>
+            </div>
+            <div className="right">
+                <div className="needs-attention-bubble"></div>
+                <span class="arrow-icon material-icons-round">navigate_next</span>
+            </div>
+        </div>
+    )
+}
+
+const MainAsideMenu: Component = () => {
+    return (
+        <div id="mainroute-aside-container">
+            <div className="aside">
+                <div className="content">
+                    <div className="userspace">
+                        <div className="main">
+                            <div className="left">
+                                <div className="user">
+                                    <div
+                                        className="user-avatar"
+                                        style={{ "background-image": "url(https://images.unsplash.com/photo-1604076913837-52ab5629fba9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)" }}
+                                    ></div>
+                                    <div className="user-username">admin</div>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div className="notifications-container has-unread">
+                                    <div className="notifications-count-container">
+                                        <div className="count">43</div>
+                                        <div className="icons">
+                                            <span class="material-icons">warning</span>
+                                            <span class="material-icons">question_mark</span>
+                                            <span class="material-icons">north_east</span>
+                                        </div>
+                                    </div>
+                                    <div className="notifications-icon">
+                                        <div className="bubble"></div>
+                                        <span class="material-icons-round">notifications</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="menu">
+                        <div className="links">
+                            <MenuLink
+                                icon_name="home"
+                                name="Home"
+                                description="Main page"
+                            />
+                            <MenuLink
+                                icon_name="dashboard"
+                                name="Dashboard"
+                                description="Instance status at a glance"
+                            />
+                            <MenuLink
+                                icon_name="tune"
+                                name="Settings"
+                                description="Instance configuration centre"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MainAsideMenu;
+
