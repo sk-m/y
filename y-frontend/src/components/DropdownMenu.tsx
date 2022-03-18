efa47f49c2d86c0a7b16b07076212e9fb1a0fe90
Max <m@ax.mk>
2022-03-18 18:15:19 +0200

    Small changes to the `DropdownMenu` component

--- y-frontend/src/components/DropdownMenu.tsx
+++ y-frontend/src/components/DropdownMenu.tsx
@@ -1,7 +1,7 @@
 import { Component } from "solid-js";
 import { useNavigate } from "solid-app-router";
 
-import styles from "./DropdownMenu.module.css";
+import "./DropdownMenu.css";
 
 export const DropdownMenuLink: Component<{
     text: string,
@@ -20,14 +20,14 @@ export const DropdownMenuLink: Component<{
     }
 
     return (
-        <div onclick={ onLinkClick } classList={{ [styles.link]: true, [styles.c_red]: props.is_red }}>
-            <div class={ styles.left }>
+        <div onclick={ onLinkClick } classList={{ "link": true, "c-red": props.is_red || false }}>
+            <div class="left">
                 {/* <div class={ styles.icon }>
                     <span class="material-icons">{ props.icon_name }</span>
                 </div> */}
-                <div class={ styles.text }>{ props.text }</div>
+                <div class="text">{ props.text }</div>
             </div>
-            <div class={ styles.right }></div>
+            <div class="right"></div>
         </div>
     )
 }
@@ -36,9 +36,9 @@ const DropdownMenu: Component<{
     shown?: boolean
 }> = props => {
     return (
-        <div classList={{ [styles.dropdown_menu_container]: true, [styles.shown]: props.shown }}>
-            <div class={ styles.dropdown_menu }>
-                <div class={ styles.links }>
+        <div classList={{ "ui-dropdown-menu-container": true, shown: props.shown }}>
+            <div class="ui-dropdown-menu">
+                <div class="links">
                     { props.children }
                 </div>
             </div>
