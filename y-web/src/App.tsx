6c733fad3b2cbb201cb7c9874c20a4f554149372
max <m@ax.mk>
2023-08-06 03:37:39 +0300

    add solid-query

--- y-web/src/App.tsx
+++ y-web/src/App.tsx
@@ -1,18 +1,23 @@
 import type { Component } from "solid-js"
 
 import { Route, Router, Routes } from "@solidjs/router"
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query"
 
 import { AppLayout } from "@/app/app-layout"
 import LoginPage from "@/modules/core/pages/login/login"
 
+const queryClient = new QueryClient()
+
 const App: Component = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/*" component={AppLayout} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/*" component={AppLayout} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
