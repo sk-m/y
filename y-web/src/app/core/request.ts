52f1cd5766badc22042a89a1c46beaa86d971287
max <m@ax.mk>
2023-08-13 00:11:00 +0300

    add api request methods

---
+++ y-web/src/app/core/request.ts
@@ -0,0 +1,58 @@
+import { Tail } from "@/app/core/utils"
+
+type RequestMethod = "GET" | "POST" | "PUT" | "DELETE"
+
+export type RequestData = {
+  query?: URLSearchParams
+  body?: Record<string, unknown>
+}
+
+export type ResponseError = {
+  code: string
+}
+
+export type ResponseData = {
+  error?: ResponseError
+} & Record<string, unknown>
+
+const request = async (
+  method: RequestMethod,
+  endpoint: string,
+  data?: RequestData,
+  requestOptions?: RequestInit
+) => {
+  const path = data?.query ? `${endpoint}?${data.query.toString()}` : endpoint
+
+  const response = await fetch(`/api${path}`, {
+    method: method,
+    mode: "cors",
+    credentials: "same-origin",
+    referrerPolicy: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: data?.body && JSON.stringify(data.body),
+
+    ...requestOptions,
+  })
+
+  return response.json().then((json: ResponseData) => {
+    if (json.error) throw json.error
+    return json
+  })
+}
+
+type RequestParameters = Tail<Parameters<typeof request>>
+
+const createRequest =
+  (method: RequestMethod) =>
+  async (...args: RequestParameters) =>
+    request(method, ...args)
+
+export const { get, post, put, del } = {
+  get: createRequest("GET"),
+  post: createRequest("POST"),
+  put: createRequest("PUT"),
+  del: createRequest("DELETE"),
+}
--- y-web/src/app/core/utils.ts
+++ y-web/src/app/core/utils.ts
@@ -1,5 +1,9 @@
 import { DEV } from "solid-js"
 
+export type Tail<T extends unknown[]> = T extends [unknown, ...infer R]
+  ? R
+  : never
+
 export const debug = (...args: unknown[]) => {
   if (DEV) console.debug(...args)
 }
