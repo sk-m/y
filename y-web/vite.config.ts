f278eebee71c3ba5ad999a4ebed1725aae626f4b
max <m@ax.mk>
2023-08-13 01:13:49 +0300

    add vite server proxy

--- y-web/vite.config.ts
+++ y-web/vite.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
   },
   build: {
     target: "esnext",
