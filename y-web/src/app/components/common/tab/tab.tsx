409c2d1c984e8156a93f03f273cf340a3989759b
max <m@ax.mk>
2023-12-09 00:06:41 +0200

    add `Tab` component

---
+++ y-web/src/app/components/common/tab/tab.tsx
@@ -0,0 +1,27 @@
+import { Component, JSX } from "solid-js"
+
+import { ComponentWithChildren } from "@/module"
+
+import "./tab.less"
+
+export type TabProps = {
+  label: JSX.Element
+  selected?: boolean
+
+  onClick?: (event: MouseEvent) => void
+}
+
+export const TabsContainer: ComponentWithChildren = (props) => {
+  return <div class="ui-tabs-container">{props.children}</div>
+}
+
+export const Tab: Component<TabProps> = (props) => {
+  return (
+    <button
+      classList={{ "ui-tab": true, selected: props.selected ?? false }}
+      onClick={(event) => props.onClick?.(event)}
+    >
+      <div class="label">{props.label}</div>
+    </button>
+  )
+}
