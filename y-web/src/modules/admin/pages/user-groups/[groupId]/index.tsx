7c70b62b012564e175a2b6f223ee6baa73d65f8c
max <m@ax.mk>
2023-12-11 00:41:43 +0200

    update user group page

--- y-web/src/modules/admin/pages/user-groups/[groupId]/index.tsx
+++ y-web/src/modules/admin/pages/user-groups/[groupId]/index.tsx
@@ -4,22 +4,35 @@ import {
   Show,
   createEffect,
   createMemo,
+  createSignal,
   untrack,
 } from "solid-js"
 
 import { useNavigate, useParams } from "@solidjs/router"
-import { createMutation } from "@tanstack/solid-query"
+import { createMutation, useQueryClient } from "@tanstack/solid-query"
 
 import { Button } from "@/app/components/common/button/button"
 import { Card } from "@/app/components/common/card/card"
+import { ExpandButton } from "@/app/components/common/expand-button/expand-button"
+import {
+  ExpandButtonEntries,
+  ExpandButtonEntry,
+} from "@/app/components/common/expand-button/expand-button-entry"
 import { Icon } from "@/app/components/common/icon/icon"
 import { Container } from "@/app/components/common/layout/container"
+import { Link } from "@/app/components/common/link/link"
+import { Modal } from "@/app/components/common/modal/modal"
 import { Stack } from "@/app/components/common/stack/stack"
 import { Text } from "@/app/components/common/text/text"
 import { useForm } from "@/app/core/use-form"
+import { routes } from "@/app/routes"
 import { updateUserGroup } from "@/modules/admin/user-groups/user-groups.api"
 import { IUserGroupRightOptionValue } from "@/modules/admin/user-groups/user-groups.codecs"
-import { useUserGroup } from "@/modules/admin/user-groups/user-groups.service"
+import {
+  useUserGroup,
+  userGroupKey,
+  userGroupsKey,
+} from "@/modules/admin/user-groups/user-groups.service"
 import { useUserRights } from "@/modules/admin/user-rights/user-rights.service"
 
 import { UserGroupRight } from "../../../components/user-group/user-group-right"
@@ -34,6 +47,11 @@ export type UserGroupWatchedFields = ["right:*", "right_option:*"]
 const UserGroupPage: Component = () => {
   const params = useParams()
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
+
+  let formRef: HTMLFormElement | undefined
+
+  const [confirmationModalOpen, setConfirmationModalOpen] = createSignal(false)
 
   const $updateUserGroup = createMutation(updateUserGroup)
 
@@ -89,7 +107,11 @@ const UserGroupPage: Component = () => {
       },
       {
         onSuccess: () => {
-          void $userGroup.refetch()
+          void queryClient.invalidateQueries([userGroupsKey])
+          void queryClient.invalidateQueries([userGroupKey, $userGroup.data.id])
+        },
+        onSettled: () => {
+          setConfirmationModalOpen(false)
         },
       }
     )
@@ -121,71 +143,159 @@ const UserGroupPage: Component = () => {
   })
 
   return (
-    <Show when={$userGroup.data}>
-      <Container size="m">
-        <Stack spacing="2em">
-          <Text
-            variant="h1"
-            style={{
-              display: "flex",
-              "align-items": "center",
-              gap: "0.5em",
-            }}
-          >
-            <Icon name="groups" grad={25} wght={500} />
-            {$userGroup.data?.name ?? "Loading..."}
+    <>
+      <Modal
+        open={confirmationModalOpen()}
+        keepMounted
+        onClose={() => setConfirmationModalOpen(false)}
+        style={{
+          "max-width": "450px",
+        }}
+        header={
+          <Stack spacing={"1.5em"} direction="row" alignItems="center">
+            <div
+              style={{
+                display: "flex",
+                "align-items": "center",
+                "justify-content": "center",
+
+                padding: "1em",
+
+                "background-color": "var(--color-border-15)",
+                "border-radius": "15px",
+              }}
+            >
+              <Icon grad={25} wght={500} size={24} name="warning" />
+            </div>
+            <Stack spacing="0.5em">
+              <Text
+                variant="h2"
+                style={{
+                  margin: 0,
+                }}
+                color="var(--color-text-grey-025)"
+              >
+                Confirm changes
+              </Text>
+            </Stack>
+          </Stack>
+        }
+      >
+        <Stack spacing={"1.5em"}>
+          <Text>
+            Are you sure you want to commit your changes to this group?
           </Text>
 
-          <form onSubmit={submit}>
-            <Stack spacing="2em">
-              <For each={userRights().categories}>
-                {(category) => (
-                  <UserGroupRightCategory category={category}>
-                    <Stack>
-                      <For each={category.rights}>
-                        {(right) => (
-                          <>
-                            <hr />
-                            <UserGroupRight right={right} form={form} />
-                          </>
-                        )}
-                      </For>
-                    </Stack>
-                  </UserGroupRightCategory>
-                )}
-              </For>
+          <Text>
+            All users that are assigned to this group will be affected
+            immediately.
+          </Text>
 
-              <Card>
-                <Stack
-                  direction="row"
-                  spacing="1em"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Button
-                    variant="secondary"
-                    disabled={$updateUserGroup.isLoading}
-                    onClick={() => navigate("/admin/user-groups")}
-                  >
-                    Back
-                  </Button>
-                  <Stack direction="row" spacing="1em">
-                    <Button
-                      buttonType="submit"
-                      disabled={$updateUserGroup.isLoading}
-                    >
-                      {$updateUserGroup.isLoading
-                        ? "Saving..."
-                        : "Save changes"}
-                    </Button>
-                  </Stack>
-                </Stack>
-              </Card>
-            </Stack>
-          </form>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            style={{
+              "margin-top": "1.5em",
+            }}
+          >
+            <Button
+              variant="secondary"
+              onClick={() => setConfirmationModalOpen(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={$updateUserGroup.isLoading}
+              onClick={() => {
+                void formRef?.dispatchEvent(new Event("submit"))
+              }}
+            >
+              {$updateUserGroup.isLoading ? "Saving..." : "Confirm"}
+            </Button>
+          </Stack>
         </Stack>
-      </Container>
-    </Show>
+      </Modal>
+      <Show when={$userGroup.data}>
+        <Container size="m">
+          <Stack spacing="2em">
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text
+                variant="h1"
+                style={{
+                  display: "flex",
+                  "align-items": "center",
+                  gap: "0.5em",
+                }}
+              >
+                <Icon name="groups" grad={25} wght={500} />
+                {$userGroup.data?.name ?? ""}
+              </Text>
+
+              <ExpandButton icon="bolt" label="Actions" position="left">
+                <ExpandButtonEntries>
+                  <ExpandButtonEntry icon="edit">Rename</ExpandButtonEntry>
+                  <ExpandButtonEntry icon="delete" variant="danger">
+                    Delete
+                  </ExpandButtonEntry>
+                </ExpandButtonEntries>
+              </ExpandButton>
+            </Stack>
+
+            <Text variant="h2">Group Rights</Text>
+
+            <form ref={formRef as HTMLFormElement} onSubmit={submit}>
+              <Stack spacing="2em">
+                <For each={userRights().categories}>
+                  {(category) => (
+                    <UserGroupRightCategory category={category}>
+                      <Stack>
+                        <For each={category.rights}>
+                          {(right) => (
+                            <>
+                              <hr />
+                              <UserGroupRight right={right} form={form} />
+                            </>
+                          )}
+                        </For>
+                      </Stack>
+                    </UserGroupRightCategory>
+                  )}
+                </For>
+
+                <Card>
+                  <Stack
+                    direction="row"
+                    spacing="1em"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Button
+                      variant="secondary"
+                      disabled={$updateUserGroup.isLoading}
+                      onClick={() => navigate(routes["/admin/user-groups"])}
+                    >
+                      Back
+                    </Button>
+                    <Stack direction="row" spacing="1em">
+                      <Button
+                        disabled={$updateUserGroup.isLoading}
+                        onClick={() => setConfirmationModalOpen(true)}
+                      >
+                        Save changes
+                      </Button>
+                    </Stack>
+                  </Stack>
+                </Card>
+              </Stack>
+            </form>
+          </Stack>
+        </Container>
+      </Show>
+    </>
   )
 }
 
