4e0eceb3d2a3cc10e2286f700ed6a59172de0112
max <m@ax.mk>
2024-01-08 01:22:33 +0200

    add an install script for the server

---
+++ y-server/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "This script will set up a service for y-server."
+echo
+echo "This directory ($(pwd)) will be set as the working directory for y."
+echo "Please make sure to move y-server's files to a desired location before continuing."
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo
+  echo "Warning: This script should be run as root!"
+fi
+
+echo
+read -p "Press enter to start: "
+
+service_definition="[Unit]
+Description=y server
+Before=postgresql.service
+
+[Install]
+WantedBy=default.target
+
+[Service]
+WorkingDirectory=$(pwd)
+ExecStart=$(pwd)/y-server
+"
+
+echo "$service_definition" > /etc/systemd/system/y-server.service
+systemctl daemon-reload
+
+echo
+echo "Service written to /etc/systemd/system/y-server.service"
+echo
+echo "To start y-server, run 'systemctl start y-server'"
+echo "You can also make y-server start on boot with 'systemctl enable y-server'"
