ee12e24ea37043112e4991f57320f5d997688d73
max <m@ax.mk>
2024-01-12 19:26:04 +0200

    add about page to the aside menu

--- y-web/src/modules/admin/layout/admin-layout.tsx
+++ y-web/src/modules/admin/layout/admin-layout.tsx
@@ -2,6 +2,7 @@ import { Component, Show, createMemo, lazy } from "solid-js"
 
 import { Route, Routes } from "@solidjs/router"
 
+import { Stack } from "@/app/components/common/stack/stack"
 import { AppAside } from "@/app/layout/app-aside"
 import { AppContent } from "@/app/layout/app-content"
 import { AsideEntry } from "@/app/layout/components/aside-entry"
@@ -29,6 +30,8 @@ const NewUserGroupPage = lazy(
   async () => import("@/modules/admin/pages/user-groups/new")
 )
 
+const AboutPage = lazy(async () => import("@/modules/admin/pages/about"))
+
 const FeaturesPage = lazy(async () => import("@/modules/admin/pages/features"))
 
 const AdminLayout: Component = () => {
@@ -52,44 +55,55 @@ const AdminLayout: Component = () => {
   return (
     <>
       <AppAside>
-        <AsideSection>
-          <AsideEntry
-            icon="person"
-            title="Users & Groups"
-            to="users"
-            relatedPaths={["users", "user-groups"]}
-          >
-            <AsideEntry subEntry icon="group" title="Users" to="users" />
+        <Stack
+          style={{
+            height: "100%",
+          }}
+          justifyContent="space-between"
+        >
+          <AsideSection>
             <AsideEntry
-              subEntry
-              icon="groups"
-              title="Groups"
-              to="user-groups"
-            />
-          </AsideEntry>
-
-          <Show when={storagePageAllowed()}>
-            <AsideEntry
-              icon="hard_drive"
-              title="Storage"
-              to="storage/endpoints"
-              relatedPaths={["storage/endpoints"]}
+              icon="person"
+              title="Users & Groups"
+              to="users"
+              relatedPaths={["users", "user-groups"]}
             >
+              <AsideEntry subEntry icon="group" title="Users" to="users" />
               <AsideEntry
                 subEntry
-                icon="data_table"
-                title="Endpoints"
-                to="storage/endpoints"
+                icon="groups"
+                title="Groups"
+                to="user-groups"
               />
             </AsideEntry>
-          </Show>
 
-          <AsideEntry icon="settings" title="Instance config" to="config" />
+            <Show when={storagePageAllowed()}>
+              <AsideEntry
+                icon="hard_drive"
+                title="Storage"
+                to="storage/endpoints"
+                relatedPaths={["storage", "storage/endpoints"]}
+              >
+                <AsideEntry
+                  subEntry
+                  icon="data_table"
+                  title="Endpoints"
+                  to="storage/endpoints"
+                />
+              </AsideEntry>
+            </Show>
 
-          <Show when={featuresPageAllowed()}>
-            <AsideEntry icon="bolt" title="Features" to="features" />
-          </Show>
-        </AsideSection>
+            <AsideEntry icon="tune" title="Instance config" to="config" />
+
+            <Show when={featuresPageAllowed()}>
+              <AsideEntry icon="bolt" title="Features" to="features" />
+            </Show>
+          </AsideSection>
+
+          <AsideSection>
+            <AsideEntry icon="info" title="About" to="about" />
+          </AsideSection>
+        </Stack>
       </AppAside>
       <AppContent>
         <Routes>
@@ -100,6 +114,7 @@ const AdminLayout: Component = () => {
           <Route path="/users/:userId/*" component={UserPage} />
           <Route path="/users/new" component={NewUserPage} />
           <Route path="/features" component={FeaturesPage} />
+          <Route path="/about" component={AboutPage} />
         </Routes>
       </AppContent>
     </>
