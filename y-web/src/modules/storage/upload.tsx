4a701c55e7fd418a91b6a8d543ec232d1e1102b2
max <m@ax.mk>
2024-01-27 16:25:38 +0200

    fix clamped folder entries resolution upon upload
    Fix a bug where folders with more than 100 files inside were not being processed correctly.
    
    On Chrome, the native `readEntries` function only returns 100 entries max. Multiple calls are required in order to retrieve all the folder contents.

--- y-web/src/modules/storage/upload.tsx
+++ y-web/src/modules/storage/upload.tsx
@@ -1,11 +1,13 @@
 export type FileWithPath = File & { path: string }
 
+// TODO this is experimental. Refactor before the next release
 export const retrieveFiles = async (
   item: Partial<FileSystemFileEntry> & Partial<FileSystemDirectoryEntry>,
   array: FileWithPath[] = [],
   path = ""
 ): Promise<FileWithPath[]> => {
-  return new Promise((resolve) => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises, no-async-promise-executor
+  return new Promise(async (resolve) => {
     if (item.isFile && item.file) {
       item.file((file) => {
         // prettier-ignore
@@ -14,18 +16,42 @@ export const retrieveFiles = async (
       })
       // eslint-disable-next-line sonarjs/elseif-without-else
     } else if (item.isDirectory && item.createReader) {
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      item.createReader().readEntries(async (entries) => {
-        for (const entry of entries) {
-          // eslint-disable-next-line require-atomic-updates, no-await-in-loop, no-param-reassign
-          array = await retrieveFiles(
-            entry,
-            array,
-            `${path + (item.name ?? "")}/`
-          )
+      const reader = item.createReader()
+      let continueReading = true
+      let lastBatchSize = 0
+
+      const readAndAppend = async (): Promise<FileWithPath[]> => {
+        return new Promise((resolveRead) => {
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          reader.readEntries(async (entries) => {
+            for (const entry of entries) {
+              // eslint-disable-next-line require-atomic-updates, no-await-in-loop, no-param-reassign
+              array = await retrieveFiles(
+                entry,
+                array,
+                `${path + (item.name ?? "")}/`
+              )
+            }
+
+            resolveRead(array)
+          })
+        })
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      while (continueReading) {
+        // eslint-disable-next-line no-await-in-loop
+        const batch = await readAndAppend()
+
+        if (lastBatchSize === batch.length) {
+          continueReading = false
+          break
         }
-        resolve(array)
-      })
+
+        lastBatchSize = batch.length
+      }
+
+      resolve(array)
     }
   })
 }
