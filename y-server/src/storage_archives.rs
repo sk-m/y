3ca0a6736fa47c2ea09cb6fbded5cc0e7e08dc48
max <m@ax.mk>
2024-02-19 23:50:53 +0200

    fix `cleanup_storage_archives`

--- y-server/src/storage_archives.rs
+++ y-server/src/storage_archives.rs
@@ -3,80 +3,37 @@ use std::{fs::remove_file, path::Path};
 use crate::util::RequestPool;
 use log::*;
 
-#[derive(sqlx::FromRow)]
-struct PartialStorageEndpointRow {
-    id: i32,
-    base_path: String,
-}
-
-#[derive(sqlx::FromRow)]
-struct PartialStorageArchiveRow {
-    endpoint_id: i32,
-    filesystem_id: String,
-}
-
 pub async fn cleanup_storage_archives(pool: &RequestPool) {
     info!("[scheduled] Cleaning up expired storage archives...");
 
-    let endpoints = sqlx::query_as::<_, PartialStorageEndpointRow>(
-        "SELECT id, base_path FROM storage_endpoints",
-    )
-    .fetch_all(pool)
-    .await;
-
-    if endpoints.is_err() {
-        error!(
-            "[scheduled] Could not fetch storage endpoints from the database. {}",
-            endpoints.err().unwrap()
-        );
-        return;
-    }
-
-    let endpoints = endpoints.unwrap();
-
-    let latest_date = chrono::Utc::now().naive_utc() - chrono::Duration::hours(24);
+    let latest_valid_date = chrono::Utc::now().naive_utc() - chrono::Duration::hours(24);
 
     let mut transaction = pool.begin().await.unwrap();
 
-    let delete_storage_archives = sqlx::query_as::<_, PartialStorageArchiveRow>(
-        "DELETE FROM storage_archives WHERE created_at < $1 RETURNING filesystem_id, endpoint_id",
+    let delete_storage_archives = sqlx::query_scalar::<_, String>(
+        "DELETE FROM storage_archives WHERE created_at < $1 RETURNING filesystem_id",
     )
-    .bind(latest_date)
+    .bind(latest_valid_date)
     .fetch_all(&mut *transaction)
     .await;
 
     match delete_storage_archives {
         Ok(delete_storage_archives) => {
-            for file in delete_storage_archives.into_iter() {
-                let file_endpoint = endpoints
-                    .iter()
-                    .find(|endpoint| endpoint.id == file.endpoint_id);
+            for file_filesystem_id in delete_storage_archives.into_iter() {
+                let file_path = Path::new("upload_staging")
+                    .join(&file_filesystem_id)
+                    .to_str()
+                    .unwrap()
+                    .to_string();
 
-                match file_endpoint {
-                    Some(endpoint) => {
-                        let file_path = Path::new(endpoint.base_path.as_str())
-                            .join(&file.filesystem_id)
-                            .to_str()
-                            .unwrap()
-                            .to_string();
+                let remove_file_result = remove_file(file_path);
 
-                        let remove_file_result = remove_file(file_path);
-
-                        if remove_file_result.is_err() {
-                            error!(
-                                "[scheduled] Could not remove an archive file from the filesystem (filesystem_id = {}). {}",
-                                file.filesystem_id,
-                                remove_file_result.err().unwrap()
-                            );
-                        }
-                    }
-                    None => {
-                        error!(
-                            "[scheduled] Could not find a storage endpoint with id {}",
-                            file.endpoint_id
-                        );
-                        continue;
-                    }
+                if remove_file_result.is_err() {
+                    error!(
+                        "[scheduled] Could not remove an archive file from the filesystem (filesystem_id = {}). {}",
+                        file_filesystem_id,
+                        remove_file_result.err().unwrap()
+                    );
                 }
             }
         }
