a5c05a7a836bbc9ff5ccd9060e2d108727f3ed16
max <m@ax.mk>
2024-02-27 12:58:08 +0200

    remove rights check for `admin/features` endpoint

--- y-server/src/api/admin/features.rs
+++ y-server/src/api/admin/features.rs
@@ -3,7 +3,6 @@ use serde::Serialize;
 
 use crate::request::error;
 
-use crate::user::get_client_rights;
 use crate::util::RequestPool;
 
 #[derive(Serialize)]
@@ -24,18 +23,7 @@ struct FeatureRow {
 }
 
 #[get("/features")]
-async fn features(pool: web::Data<RequestPool>, req: actix_web::HttpRequest) -> impl Responder {
-    let client_rights = get_client_rights(&pool, &req).await;
-
-    let action_allowed = client_rights
-        .iter()
-        .find(|right| right.right_name.eq("update_features"))
-        .is_some();
-
-    if !action_allowed {
-        return error("features.unauthorized");
-    }
-
+async fn features(pool: web::Data<RequestPool>) -> impl Responder {
     let feature_rows = sqlx::query_as::<_, FeatureRow>("SELECT feature, enabled FROM features")
         .fetch_all(&**pool)
         .await;
