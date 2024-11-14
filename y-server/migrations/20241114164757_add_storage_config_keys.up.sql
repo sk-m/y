f41cbc9532e360fc474e8f3b2d80ee064d9a7f57
max <m@ax.mk>
2024-11-14 21:01:44 +0200

    add storage config keys

---
+++ y-server/migrations/20241114164757_add_storage_config_keys.up.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.config (key,value) VALUES
+  ('storage.generate_thumbnails.image','true'),
+  ('storage.generate_thumbnails.video','true'),
+  ('storage.generate_thumbnails.audio','true'),
+  ('storage.generate_seeking_thumbnails.enabled','true'),
+  ('storage.generate_seeking_thumbnails.desired_frames','9'),
+  ('storage.transcode_videos.enabled','true'),
+  ('storage.transcode_videos.target_height','720'),
+  ('storage.transcode_videos.target_bitrate','4000');
